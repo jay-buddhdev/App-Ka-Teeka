c5af12a570d3b23c40aa5457bf73a6286c1f79b4
Jay Buddhdev <jaybuddhdev@Jays-MacBook-Pro.local>
2022-04-12 22:58:42 +0530

    Java Files Added

---
+++ src/java/DatabaseConstants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jaybuddhdev
+ */
+public class DatabaseConstants {
+    public static String driverName = "com.mysql.cj.jdbc.Driver";
+    public static String connectionString = "jdbc:mysql://localhost:3306/apnatekka?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    public static String UserName = "root";
+    public static String Password = "";
+}
